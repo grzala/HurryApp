27abaf1c753385ef4e32378f9c89298907e6277b
Grant Christie <g.christie.14@aberdeen.ac.uk>
2015-10-21 14:26:19 +0100

    finished model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+	validates :name, :presence => true, :uniqueness => true
+	validates :email, :presence => true, :uniqueness => true
+	
+	validates :password, :confirmation => true
+	attr_accesor :password_confirmation
+	attr_reader :password
+	
+	validate :password_must_be_present
+	
+	private
+	
+	def password_must_be_present
+		errors.add(:password, "Missing Password") unless hashed_password.present?
+	end
 end
