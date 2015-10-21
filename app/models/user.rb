7009c84ef478ea8d94c2377be9c415ef9682178f
Grant Christie <g.christie.14@aberdeen.ac.uk>
2015-10-21 14:31:36 +0100

    more authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha2'
+
 class User < ActiveRecord::Base
 	validates :name, :presence => true, :uniqueness => true
 	validates :email, :presence => true, :uniqueness => true
@@ -13,4 +15,30 @@ class User < ActiveRecord::Base
 	def password_must_be_present
 		errors.add(:password, "Missing Password") unless hashed_password.present?
 	end
+	
+	def user.encrypt_password(password, salt)
+		Digest::SHA2.hexdigest(password + "wibble" + salt)
+	end
+	
+	def generate_salt
+		self.salt = self.object_id.to_s + rand.to_s
+	end
+	
+	def password=(password)
+		@password = password
+		
+		if password.present?
+			generate_salt
+			self.hashed_password = self.class.encrypt_password(password, salt)
+		end
+	end
+	
+	def User.authenticate(name, password)
+		if user = find_by_name(name)
+			if user.hashed_password == encrypt_password(password, salt)
+				user
+			end
+		end
+	end
+	
 end
