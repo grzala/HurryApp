697180a974fd388ad4d3c69aebdc379b2348bdea
grzala <m.panasiuk.14@outlook.com>
2015-10-24 19:20:48 +0000

    prevent comments with no date,message, user or toilet

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
-
+    validates :toilet_id, :presence => true
+    validates :user_id, :presence => true
+    validates :message, :presence => true, length: {maximum: 300}
+    validates :date, :presence => true
 end
