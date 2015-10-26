1f98b8d1d3a6b8dab48925be7424562ade786a74
Marcel Zak <marcel.zak.13@gmail.com>
2015-10-26 18:04:42 +0000

    app is using psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151024114011) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "toilet_id"
     t.integer  "user_id"
