d5161289743d24e358f2b61eefb7b65c6a9aca4b
grzala <m.panasiuk.14@outlook.com>
2015-10-27 16:29:45 +0000

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.new(:name => "test", :password => "testtest", :email => "test@test.com")
+@user.save
+
+@user = User.new(:name => "test2", :password => "test2test2", :email => "test2@test.com")
+@user.save
+
+@user = User.new(:name => "test3", :password => "test3test3", :email => "test3@test.com")
+@user.save
+
+@admin = User.new(:name => "admin", :password => "adminadmin", :email => "admin@test.com", :admin => true)
+@admin.save
